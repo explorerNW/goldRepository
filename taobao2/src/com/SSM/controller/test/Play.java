6aaaae8377d228fccc0c842aedeaea61489ea1e9
explorerNW <superexplorernw@163.com>
2017-10-10 22:07:56 +0800

    m

--- taobao2/src/com/SSM/controller/test/Play.java
+++
@@ -1,9 +0,0 @@
-package com.SSM.controller.test;
-
-public interface Play extends Higher{
-                     public void playing();
-                    @Override
-                    default void higher() {
-                               System.out.println("high!");                    		
-                    }
-}
