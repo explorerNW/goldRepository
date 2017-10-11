b7ebfdc8f4f24f2a2bfcef7ed9779e6444007d22
explorerNW <superexplorernw@163.com>
2017-10-11 20:20:43 +0800

    modified:   taobao2/WebRoot/scripts/function-manage.js

--- taobao2/WebRoot/scripts/function-manage.js
+++ taobao2/WebRoot/scripts/function-manage.js
@@ -1,7 +1,7 @@
 // JavaScript Document
 function Delete(id)
 {
-	if(confirm("确定要删除吗？")) {
+	if(confirm("确定要删除吗？Are you sure to delete this?")) {
 		location.href = "user-delete.html?id=" + id;
 	}
 }
